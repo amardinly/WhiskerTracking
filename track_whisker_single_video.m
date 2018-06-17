3806bb49af86726ac686d015b0be1b9adf496f11
Alan Mardinly <alan.mardinly@gmail.com>
2018-06-17 15:32:09 -0700

    many improvements to tracking
    1) catchDelta lowered to  75
    2) max count on blobs increased to 10.
    3) changed how the cleaning post-binarization is done to be one round of imerode and one round of imclose.
    4) removed imadjust
    5) added a while loop to search for best sensitivity

--- track_whisker_single_video.m
+++ track_whisker_single_video.m
@@ -1,5 +1,6 @@
 function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_display, detail_timing, do_save)
-    catchDelta = 150;
+    catchDelta = 75;
+    sensitivity_base = .15;
     tic
     %% load tiff
     %FinalImage=ScanImageTiffReader(FileTif).data;
@@ -16,6 +17,8 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
     nu = single(FinalImage);
     nu = nu - min(nu(:));
     nu = nu./max(nu(:));
+    bg = mean(cat(3,mean(nu,3), bg),3);
+
     if detail_timing; disp('preprocess'); toc; tic; end
 
     %% variable to store centroids in
@@ -24,15 +27,15 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
     %% set up the blob and drawing fxn
     blob = vision.BlobAnalysis(...
            'BoundingBoxOutputPort', true, ...
-           'MinimumBlobAreaSource', 'Property', 'MinimumBlobArea', 20, 'MaximumBlobArea', 5000, 'MaximumCount', 2,...
+           'MinimumBlobAreaSource', 'Property', 'MinimumBlobArea', 20, 'MaximumBlobArea', 5000, 'MaximumCount', 10,...
        'ExcludeBorderBlobs',false);
     blob_strict = vision.BlobAnalysis(...
            'BoundingBoxOutputPort', true, ...
-           'MinimumBlobAreaSource', 'Property', 'MinimumBlobArea', 100, 'MaximumBlobArea', 5000, 'MaximumCount', 2,...
+           'MinimumBlobAreaSource', 'Property', 'MinimumBlobArea', 100, 'MaximumBlobArea', 5000, 'MaximumCount', 10,...
        'ExcludeBorderBlobs',true);
-    shapeInserter = vision.ShapeInserter('BorderColor','White');
     %for speed, precalculate this strel thing
-    disk_rad_2 = strel('disk',2,8);
+    disk_rad_1 = strel('disk',1,8);
+    disk_rad_4 = strel('disk',2,8);
     %% first get the starting blob.
     %If not found in first frame, keep searching till it is found
     found_first = false;
@@ -44,21 +47,23 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
             disp('no blobs found in video')
             break %leave the while loop
         end
-        processed = process(nu(:,:,search_frame),bg(:,:));
+        processed = process(nu(50:end-50,50:end-50, search_frame), bg(50:end-50,50:end-50));
+      
         
-        threshed=binarize(processed, .15);
         cleaned=imopen(threshed+1,disk_rad_2);
-        cleaned=imopen(cleaned,disk_rad_2);
+        threshed=binarize(processed, sensitivity_base);
+        sensitivity = sensitivity_base;
+        cleaned=clean(threshed+1, disk_rad_1, disk_rad_4);
         %handle differences btwn matlab 2016 and 2017
         try
             [area, centroid, bbox] = blob_strict(logical(cleaned));
         catch
             [area, centroid, bbox] = step(blob_strict,logical(cleaned));
         end
-        if isempty(area)
-            threshed=binarize(processed, .3);
-            cleaned=imopen(threshed+1,disk_rad_2);
-            cleaned=imopen(cleaned,disk_rad_2);
+        while isempty(area) && sensitivity < .95
+            sensitivity=sensitivity+.05;
+            threshed=binarize(processed,sensitivity);
+            cleaned=clean(threshed+1, disk_rad_1, disk_rad_4);
             %handle differences btwn matlab 2016 and 2017
             try
                 [area, centroid, bbox] = blob_strict(logical(cleaned));
@@ -68,6 +73,8 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
             %imagesc(threshed)
             %pause(.1)
         end
+        %set the new sensitivity to be just below the sensitivity that worked
+        sensitivity_base = sensitivity-.05;
         if ~isempty(area)
             if length(area)>1
                 %for the first one only
@@ -84,18 +91,24 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
             centroids(:,search_frame) = centroid.';
             good_frames(search_frame)=1;
             found_first = true; %mark that we found the first centroid
-            
+        end
             if do_display
-                subplot(1,3,1)
-                subplot(1,3,2)
+                subplot(2,3,1)
+                imagesc(rerange)
+                subplot(2,3,2)
+                %imagesc(reranged);
+                subplot(2,3,3);
+                imagesc(processed);
+                subplot(2,3,4);
+                imagesc(threshed);
+                subplot(2,3,5)
                 imagesc(cleaned);
-                subplot(1,3,3)
-                imagesc(nu(:,:,search_frame));
+                subplot(2,3,6)
+                imagesc(nu(50:end-50,50:end-50,search_frame));
                 title(int2str(search_frame));
                 colormap('gray')
-                pause(4)
+                pause(.5)
             end
-        end
         search_frame = search_frame+1;
     end
     disp(['it took ' int2str(search_frame-1) ' frames to find a start']);
@@ -116,20 +129,21 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
         %make sure it fits in the image
         pixX(pixX<1)=[]; pixY(pixY<1)=[];
         pixX(pixX>size(nu,2))=[]; pixY(pixY>size(nu,1))=[];
+        processed = process(nu(pixY,pixX,i), bg(pixY,pixX));
         
-        processed = process(nu(pixY,pixX,i),bg(pixY,pixX));
-        threshed=binarize(processed, .15);
-        cleaned=imopen(threshed+1,disk_rad_2);
+        threshed=binarize(processed, sensitivity_base);
+        cleaned=clean(threshed+1, disk_rad_1, disk_rad_4);
         try
             [area, centroid, bbox] = blob(logical(cleaned));
         catch
             [area, centroid, bbox] = step(blob,logical(cleaned));
         end
-        
-        if isempty(area)
-            threshed=binarize(processed, .3);
-            cleaned=imopen(threshed+1,disk_rad_2);
-            cleaned=imopen(cleaned,disk_rad_2);
+        sensitivity = sensitivity_base;
+
+        while isempty(area) && sensitivity < .95
+            sensitivity=sensitivity+.05;
+            threshed=binarize(processed, sensitivity);
+            cleaned=clean(threshed+1, disk_rad_1, disk_rad_4);
             %handle differences btwn matlab 2016 and 2017
             try
                 [area, centroid, bbox] = blob_strict(logical(cleaned));
@@ -158,22 +172,27 @@ function [centroids, good_frames] = track_whisker_single_video(FileTif, bg, do_d
             centroids(:,i) = centroid.';
         end
         if do_display
-            subplot(1,3,1)
-            try
-                    imagesc(shapeInserter(processed,bbox));
-            catch
-                imagesc(step(shapeInserter,processed,bbox));
-            end
-            subplot(1,3,2)
-            imagesc(cleaned);
-            subplot(1,3,3)
-            imagesc(nu(:,:,i));
-            if ~isempty(bbox)
-                hold on;
-                scatter([bbox(1) bbox(1)+bbox(3)], [bbox(2) bbox(2)+bbox(4)]);
-                scatter(centroid(1), centroid(2));
-                hold off;
-            end
+            subplot(2,3,1)
+                imagesc(rerange)
+                subplot(2,3,2)
+                %imagesc(reranged);
+                subplot(2,3,3);
+                imagesc(processed);
+                subplot(2,3,4);
+                imagesc(threshed);
+                subplot(2,3,5)
+                imagesc(cleaned);
+                if ~isempty(bbox)
+                    hold on; scatter(centroid(1), centroid(2)); hold off;
+                end
+                subplot(2,3,6)
+                imagesc(nu(:,:,i));
+                if ~isempty(bbox)
+                    hold on;
+                    scatter([bbox(1) bbox(1)+bbox(3)], [bbox(2) bbox(2)+bbox(4)]);
+                    scatter(centroid(1), centroid(2));
+                    hold off;
+                end
             title(int2str(i));
             colormap('gray')
             pause(.05)
@@ -193,11 +212,18 @@ function processed = process(crop_im, crop_bg)
     rerange = crop_im-crop_bg;
     rerange = rerange-min(rerange(:));
     rerange = rerange./max(rerange(:));
-    rerange = imadjust(rerange); %enhance contrast
     processed = imgaussfilt(rerange,2); %denoise
 end
 
 
 function threshed=binarize(processed, sensitivity)
+    %threshed=-imbinarize(processed, sensitivity*mean(processed(:)));
+
     threshed=-imbinarize(processed, 'adaptive', 'Sensitivity',sensitivity, 'ForegroundPolarity', 'dark');
 end
+
+function cleaned = clean(im, disk_erode, disk_close)
+    cleaned = imerode(im, disk_erode);
+    cleaned = imclose(cleaned, disk_close);
+end
+
